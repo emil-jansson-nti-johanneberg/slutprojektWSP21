51dcef51524d643234f5ce6158e8a2dbabcfc7c9
emil-jansson-nti-johanneberg <60172954+emil-jansson-nti-johanneberg@users.noreply.github.com>
2021-03-05 12:51:47 +0100

    Update model.rb

--- model/model.rb
+++ model/model.rb
@@ -3,16 +3,13 @@ module Model
     require 'sqlite3'
     require 'bcrypt'
 
-    #This functions makes it so I don't have to repeat myself in module with these two lines of code
+    #Så jag slipper skriva detta flertalet gånger i alla modules
     def connect_to_db(path) 
         db = SQLite3::Database.new(path)
         db.results_as_hash = true
         return db
     end 
-    # Attempts to recieve information from a particular mail
-    #
-    # @option params [String] mail The e-mail
-    #
+
     def get_info_from_mail(login_mail)
 
         db = connect_to_db("db/db.db")
